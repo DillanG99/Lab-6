0424a141f17ba9437ece2965a3a0db15fa6ac945
Dillan Gibbons <Dillan.G.Gibbons-1@ou.edu>
2019-02-26 22:13:54 -0600

    Added Comments

--- Rectangle.java
+++ Rectangle.java
@@ -3,16 +3,27 @@ public class Rectangle extends Polygon {
 	private double height;
 	private double width;
 	
-	
+	/**
+	 * Creates a Rectangle object of set height and width with a user specified ID
+	 * @param id
+	 * @param height
+	 * @param width
+	 */
 	public Rectangle(String id, double height, double width) {
 		super(id);
 		this.height = height;
 		this.width = width;
 	}
+	/**
+	 * Calculates the area of the rectangle (height * width) of the rectangle object
+	 */
 	@Override
 	public double getArea() {
 		return height * width;
 	}
+	/**
+	 * @return A String respresentation of the type of shape the object is in this case: "Rectangle"
+	 */
 	@Override
 	public String getShapeType() {
 		return "Rectangle";
--- Square.java
+++ Square.java
@@ -1,9 +1,18 @@
 
 public class Square extends Rectangle {
 	
+	/**
+	 * Creates a square object
+	 * uses the recatangle constructo but sets both side lengths equal to each other
+	 * @param id
+	 * @param size
+	 */
 	public Square(String id, double size) {
 		super(id, size, size);
 	}
+	/**
+	 * @return A String respresentation of the type of shape the object is in this case: "Square"
+	 */
 	public String getShapeType() {
 		return "Square";
 	}
